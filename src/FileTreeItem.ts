3ce48ac610bdeb42c9802b083473993294b2d93a
Rockets-X <zcjb.rjdj@gmail.com>
2025-02-15 12:32:34 +0800

    Enhance FileTreeItem to support method type and improve tooltips

--- src/FileTreeItem.ts
+++ src/FileTreeItem.ts
@@ -16,12 +16,33 @@ export class FileTreeItem extends vscode.TreeItem {
   ) {
     super(label, collapsibleState);
 
-    // Add icon for files only
-    if (type === FileTreeItemType.File) {
-      this.contextValue = 'file'; // This enables context menu items
-      this.iconPath = new vscode.ThemeIcon('file');
-      // Add the test generation button in the tree item
-      this.command = command;
+    switch (type) {
+      case FileTreeItemType.Folder:
+        this.contextValue = 'folder';
+        this.iconPath = new vscode.ThemeIcon('folder');
+        break;
+      case FileTreeItemType.File:
+        this.contextValue = 'file';
+        this.iconPath = new vscode.ThemeIcon('file');
+        this.command = command;
+        break;
+      case FileTreeItemType.Method:
+        this.contextValue = 'method';
+        this.iconPath = new vscode.ThemeIcon('symbol-method');
+        this.command = command;
+        break;
+    }
+
+    // Add tooltip showing the full path for files and folders
+    if (type !== FileTreeItemType.Method) {
+      this.tooltip = fullPath;
+    }
+
+    // Add description for methods showing the file name
+    if (type === FileTreeItemType.Method) {
+      const filename = fullPath.split('/').pop() || '';
+      this.description = filename;
+      this.tooltip = `${label} in ${filename}`;
     }
   }
 }
