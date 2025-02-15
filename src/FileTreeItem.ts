61a98128a08c07109406ee676ca00993873e73b8
Rockets-X <zcjb.rjdj@gmail.com>
2025-02-15 13:11:19 +0800

    Enhance FileTreeItem to use specific icons based on file extension and improve tooltips for better context

--- src/FileTreeItem.ts
+++ src/FileTreeItem.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 export enum FileTreeItemType {
   Folder,
@@ -23,26 +24,39 @@ export class FileTreeItem extends vscode.TreeItem {
         break;
       case FileTreeItemType.File:
         this.contextValue = 'file';
-        this.iconPath = new vscode.ThemeIcon('file');
+        // Use specific icons based on file extension
+        const ext = path.extname(fullPath).toLowerCase();
+        // 使用文件类型图标
+        if (ext === '.ts' || ext === '.tsx') {
+          this.iconPath = new vscode.ThemeIcon('symbol-file');
+        } else if (ext === '.json') {
+          this.iconPath = new vscode.ThemeIcon('json');
+        } else if (fullPath.includes('node_modules')) {
+          this.iconPath = new vscode.ThemeIcon('package');
+        } else {
+          this.iconPath = new vscode.ThemeIcon('file');
+        }
         this.command = command;
+        // Show file extension in the label
+        this.label = path.basename(fullPath);
         break;
       case FileTreeItemType.Method:
         this.contextValue = 'method';
-        this.iconPath = new vscode.ThemeIcon('symbol-method');
+        // 使用更具体的方法符号图标
+        this.iconPath = new vscode.ThemeIcon('symbol-function');
         this.command = command;
         break;
     }
 
-    // Add tooltip showing the full path for files and folders
-    if (type !== FileTreeItemType.Method) {
-      this.tooltip = fullPath;
-    }
-
-    // Add description for methods showing the file name
-    if (type === FileTreeItemType.Method) {
-      const filename = fullPath.split('/').pop() || '';
+    // Enhanced tooltips
+    if (type === FileTreeItemType.Folder) {
+      this.tooltip = `📁 ${fullPath}`;
+    } else if (type === FileTreeItemType.File) {
+      this.tooltip = `📄 ${fullPath}`;
+    } else if (type === FileTreeItemType.Method) {
+      const filename = path.basename(fullPath);
       this.description = filename;
-      this.tooltip = `${label} in ${filename}`;
+      this.tooltip = `🔧 ${label} in ${filename}`;
     }
   }
 }
